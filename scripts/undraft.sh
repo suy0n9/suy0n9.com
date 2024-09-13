ae70cd8c747b79c63eea55e72dfad1206ca7608d
suy0n9 <suy01v9@gmail.com>
2024-09-14 00:16:17 +0900

    add Taskfile and fix undraft command

--- scripts/undraft.sh
+++ scripts/undraft.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
-file=$1
+file=$(find content/posts -name "*.md" | fzf --prompt="Select a draft to undraft: ")
+
+if [ -z "$file" ]; then
+    echo "No file selected. Exiting."
+    exit 1
+fi
+
 now="$(date "+%Y-%m-%dT%H:%M:%S%z" | sed -e 's/00$/:00/')"
 
 if [ -f "${file}" ]; then
     sed -i -e 's/^date: 20.*$/date: '"${now}"'/' \
-           -e 's/^draft: true$/draft: false/' "${file}"
-    echo "Update date: ${now}"
-    echo "Update draft: false"
+        -e 's/^draft: true$/draft: false/' "${file}"
+    echo "Update file: ${file}"
+    echo "Set data to: ${now}"
+    echo "Draft status set to false"
 else
     echo "${file} not found"
     exit 1
